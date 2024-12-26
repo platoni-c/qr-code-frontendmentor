65d8c2634f07bc39d54fcc9132b9b81bc78ec96b
Samuel <Flouric@Samuels-MacBook-Air.local>
2024-12-26 12:32:08 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    screens: {
+      sm: "375px",
+      md: "1440px",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
